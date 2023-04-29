49eb15d1e32a2553b9470c801c76046f55619c72
Максим <novokshonov_m@mail.ru>
2023-04-29 13:28:56 +0300

    Update. Main layout added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TableComponent } from './components/table/table.component';
+import { ClientComponent } from './components/client/client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TableComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule,
